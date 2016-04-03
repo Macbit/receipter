e2cbc8b0b4813c3ea4c4e53ce01aa530b0427c52
Nikita Samarin <n.samarin.s@gmail.com>
2016-04-03 11:44:19 +0100

    add post requests

--- client/templates/includes/header.js
+++ client/templates/includes/header.js
@@ -1,55 +1,61 @@
 Template.header.events({
-    'click .upload': function(e) {
-        e.preventDefault();
-        var self = this; //small hack to avoid loosing 'this' inside functions downstream
-        console.log('button pressed');
-        var options = {
-            width: 350,
-            height: 350,
-            quality: 75,
-            sourceType: Camera.PictureSourceType.PHOTOLIBRARY
-        }
-
-        MeteorCamera.getPicture(options, function(err, data) {
-            if (err) {
-                console.log('error', err);
-            }
-            if (data) {
-                Session.set('img', data);
-                console.log('we are inside ifDATA block of MeteorCamera');
-                //processImage(data);
-                Meteor.call('processImage', data, function(error, result) {
-                    // handle error/success
-                });
-            }
-        });
-    },
-    'click .camera': function(e) {
-        e.preventDefault();
-        var self = this; //small hack to avoid loosing 'this' inside functions downstream
-        console.log('button pressed');
-
-        var options = {
-            width: 350,
-            height: 350,
-            quality: 75
-        }
-        MeteorCamera.getPicture(options, function(err, data) {
-            if (err) {
-                console.log('error VERY BAD ERROR', err);
-            }
-            if (data) {
-                Session.set('img', data);
-                console.log(data);
-                //processImage(data);
-            }
-        });
-
+  'click .upload': function(e) {
+    e.preventDefault();
+    var self = this; //small hack to avoid loosing 'this' inside functions downstream
+    console.log('button pressed');
+    var options = {
+      width: 350,
+      height: 350,
+      quality: 75,
+      sourceType: Camera.PictureSourceType.PHOTOLIBRARY
     }
+
+    MeteorCamera.getPicture(options, function(err, data) {
+      if (err) {
+        console.log('error from get picture function', err);
+      }
+      if (data) {
+        console.log("data from get picture", data);
+        Session.set('img', data);
+        console.log('we are inside if DATA block of MeteorCamera');
+
+        Meteor.call('performOCR', data, function(error, result) {
+          //   // handle error/success
+        });
+
+        //processImage(data);
+        // Meteor.call('processImage', data, function(error, result) {
+        //   // handle error/success
+        // });
+      }
+    });
+  },
+  'click .camera': function(e) {
+    e.preventDefault();
+    var self = this; //small hack to avoid loosing 'this' inside functions downstream
+    console.log('button pressed');
+
+    var options = {
+      width: 350,
+      height: 350,
+      quality: 75
+    }
+    MeteorCamera.getPicture(options, function(err, data) {
+      if (err) {
+        console.log('error from get picture', err);
+      }
+      if (data) {
+        Session.set('img', data);
+        console.log(data);
+        //processImage(data);
+      }
+    });
+
+  }
 });
 
 Template.header.helpers({
-    mobile: function() {
-        return Meteor.isCordova
-    }
+  mobile: function() {
+    return Meteor.isCordova
+  }
 });
